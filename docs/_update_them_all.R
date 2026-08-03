7292a1b1e08c7439dc4b0cb49181523a4dced47a
Marcos Lima <marcoslima@192.168.0.3>
2026-08-03 10:15:25 -0300

    rendering with Quarto

--- docs/_update_them_all.R
+++ docs/_update_them_all.R
@@ -1,22 +1,24 @@
-library(purrr)
-library(rmarkdown)
-library(withr)
+# library(purrr)
+# library(rmarkdown)
+# library(withr)
 
 rmd_files <- list.files(
   "docs", #/path/to/project
-  pattern = "\\.Rmd$",
+  pattern = "\\.(Rmd|qmd)$",
   recursive = TRUE,
   full.names = TRUE
 )
 
-walk(rmd_files, \(abs) {
-  with_dir(dirname(abs), {
+purrr::walk(rmd_files, \(abs) {
+  # with_dir(dirname(abs), {
     message("File: ", abs)
-    render(
-      input = basename(abs),
-      output_format = "html_document",
-      clean = TRUE,
-      envir = new.env(parent = globalenv())
-    )
-  })
+    quarto::quarto_render(abs)
+    # rmarkdown::render(
+    #   input = abs,
+    #   # input = basename(abs),
+    #   # output_format = "html_document",
+    #   clean = TRUE,
+    #   envir = new.env(parent = globalenv())
+    # )
+  # })
 })
